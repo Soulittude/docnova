d67e1915123326ce2738edbd3e267ed04bd038d6
soulittude <79145776+Soulittude@users.noreply.github.com>
2025-07-09 18:46:34 +0300

    Implement PrivateRoute component for protected invoice access

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,36 @@
+import { useSelector } from "react-redux";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 import Loginpage from "./pages/LoginPage";
 import InvoiceDetailPage from "./pages/InvoiceDetailPage";
 import InvoiceListPage from "./pages/InvoiceListPage";
 
+function PrivateRoot({ children }) {
+  const user = useSelector((state) => state.auth.user);
+  return user ? children : <Navigate to="/login" replace />;
+}
+
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/login" element={<Loginpage />}></Route>
-        <Route path="/invoices" element={<InvoiceListPage />}></Route>
-        <Route path="/invoices/:id" element={<InvoiceDetailPage />}></Route>
+        <Route
+          path="/invoices"
+          element={
+            <PrivateRoute>
+              <InvoiceListPage />
+            </PrivateRoute>
+          }
+        ></Route>
+        <Route
+          path="/invoices/:id"
+          element={
+            <PrivateRoute>
+              <InvoiceDetailPage />
+            </PrivateRoute>
+          }
+        ></Route>
         <Route path="*" element={<Navigate to="/login" replace />}></Route>
       </Routes>
     </BrowserRouter>
