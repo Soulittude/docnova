25caa9bdc4110817743bf07379fe28d6cd103bdf
soulittude <79145776+Soulittude@users.noreply.github.com>
2025-07-09 16:45:44 +0300

    Refactor main entry point to use Redux Provider and set up store configuration

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import 'antd/dist/reset.css'; //
-import { DatePicker, message } from 'antd';
 import './index.css'
+import { DatePicker, message } from 'antd'; //
 import App from './App.jsx'
+import { Provider } from 'react-redux';
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  <Provider store={store}>
     <App />
-  </StrictMode>,
+  </Provider>,
 )
