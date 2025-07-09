6624d90d5b7133f582060a336264b287decc1e6f
soulittude <79145776+Soulittude@users.noreply.github.com>
2025-07-09 17:37:05 +0300

    Refactor LoginPage component to implement login functionality with error handling and navigation

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,14 @@
-import { createRoot } from 'react-dom/client'
-import 'antd/dist/reset.css'; //
-import './index.css'
-import { DatePicker, message } from 'antd'; //
-import App from './App.jsx'
-import { Provider } from 'react-redux';
-import i18n from './i18n.js';
+import { createRoot } from "react-dom/client";
+import "antd/dist/reset.css"; //
+import "./index.css";
+import App from "./App.jsx";
+import { Provider } from "react-redux";
+import i18n from "./i18n.js";
 
-
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <Provider store={store}>
-    <App />
-  </Provider>,
-)
+    <I18nextProvider i18n={i18n}>
+      <App />
+    </I18nextProvider>
+  </Provider>
+);
--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,1 +1,60 @@
-export default Loginpage;
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { login } from "../features/auth/authThunks";
+import { Navigate } from "react-router-dom";
+import { Form, Input, Button, message } from "antd";
+import FormItem from "antd/es/form/FormItem";
+
+export default function LoginPage() {
+  const dispatch = useDispatch();
+  const { user, status, error } = useSelector((state) => state.auth);
+  const [form] = Form.useForm();
+
+  const onFinish = ({ email, password }) => {
+    dispatch(login({ email, password }))
+      .unwrap()
+      .then(() => {
+        message.success("Login succeeded.");
+      })
+      .catch((msg) => {
+        message.error(msg);
+      });
+  };
+
+  if (user) return <Navigate to="/invoices" replace />;
+
+  return (
+    <Form
+      form={form}
+      layout="vertical"
+      onFinish={onFinish}
+      style={{ maxWidth: 300, margin: "auto", marginTop: "100px" }}
+    >
+      <Form.Item
+        label="E-mail"
+        name="email"
+        rules={[{ required: true, message: "Enter your email" }]}
+      >
+        <Input />
+      </Form.Item>
+      <Form.Item
+        label="Password"
+        name="password"
+        rules={[{ required: true, message: "Enter your password" }]}
+      >
+        <Input.Password />
+      </Form.Item>
+      <Form.Item>
+        <Button
+          type="primary"
+          htmlType="submit"
+          loading={status === "loading"}
+          block
+        >
+          Login
+        </Button>
+      </Form.Item>
+      {error && <div style={{ color: "red" }}>{error}</div>}
+    </Form>
+  );
+}
